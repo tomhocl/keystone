4b4614b0226c98de751e0b022901f95fed8bfd34
Josh Calder <josh.calder@thinkmill.com.au>
2022-11-24 10:07:18 +1100

    Add support for typed sessions

--- examples/roles/keystone.ts
+++ examples/roles/keystone.ts
@@ -2,8 +2,10 @@ import { config } from '@keystone-6/core';
 import { statelessSessions } from '@keystone-6/auth/session';
 import { createAuth } from '@keystone-6/auth';
 import { lists } from './schema';
-
 import { isSignedIn } from './access';
+
+import { Session } from './types';
+import { TypeInfo } from '.keystone/types';
 
 const sessionSecret = '-- DEV COOKIE SECRET; CHANGE ME --';
 const sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
@@ -51,7 +53,7 @@ const { withAuth } = createAuth({
   sessionStrategy: statelessSessions(sessionConfig),
 });
 
-export default withAuth(
+export default withAuth<TypeInfo<Session>>(
   config({
     db: {
       provider: 'sqlite',
